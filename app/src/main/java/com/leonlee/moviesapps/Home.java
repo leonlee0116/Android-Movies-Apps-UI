4029981050e9ce5b5fa15d477973d1c0dc6d61e7
Leon Lee <leonlee0116jl@gmail.com>
2020-02-04 23:33:33 +0800

    Initial commit

---
+++ app/src/main/java/com/leonlee/moviesapps/Home.java
@@ -0,0 +1,19 @@
+package com.leonlee.moviesapps;
+
+/**
+ * Inspired by Balance from Dribble
+ * https://dribbble.com/shots/6724418-Movie-app/attachments/1436036
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
