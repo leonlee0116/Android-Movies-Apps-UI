71974be939eeee08bb4f6af94e2cbf248d999f50
Lee Jun Liang <junliang@timeteccloud.com>
2020-02-05 16:47:44 +0800

    UI Update

--- app/src/main/java/com/leonlee/moviesapps/Home.java
+++ app/src/main/java/com/leonlee/moviesapps/Home.java
@@ -14,7 +14,7 @@ import com.bumptech.glide.Glide;
 
 public class Home extends AppCompatActivity {
 
-    private ImageView iv1, iv2, iv3;
+    private ImageView iv1, iv2, iv3, iv4, iv5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +24,14 @@ public class Home extends AppCompatActivity {
         iv1 = findViewById(R.id.iv1);
         iv2 = findViewById(R.id.iv2);
         iv3 = findViewById(R.id.iv3);
+        iv4 = findViewById(R.id.iv4);
+        iv5 = findViewById(R.id.iv5);
 
         Glide.with(this).load("https://assets.2ser.com/wp-content/aws/uploads/2018/02/17204756/BlackPanther.jpg").centerCrop().into(iv1);
         Glide.with(this).load("https://picsum.photos/350/200").into(iv2);
         Glide.with(this).load("https://picsum.photos/350/200").into(iv3);
+
+        Glide.with(this).load("https://is5-ssl.mzstatic.com/image/thumb/Video128/v4/f2/0b/fa/f20bfa07-96dc-e39b-d8db-1c2f8aae8482/source/600x600bb.jpg").into(iv4);
+        Glide.with(this).load("https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/c4/2a/ad/c42aadfb-764e-db99-300b-c0888604a06b/pr_source.lsr/600x600bb.jpg").into(iv5);
     }
 }
