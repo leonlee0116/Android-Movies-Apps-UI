2c780a69e52fe21e00806f068f9ce89fe1ca7261
Lee Jun Liang <junliang@timeteccloud.com>
2020-02-07 14:21:03 +0800

    UI Update

--- app/src/main/java/com/leonlee/moviesapps/Home.java
+++ app/src/main/java/com/leonlee/moviesapps/Home.java
@@ -28,8 +28,8 @@ public class Home extends AppCompatActivity {
         iv5 = findViewById(R.id.iv5);
 
         Glide.with(this).load("https://assets.2ser.com/wp-content/aws/uploads/2018/02/17204756/BlackPanther.jpg").centerCrop().into(iv1);
-        Glide.with(this).load("https://picsum.photos/350/200").into(iv2);
-        Glide.with(this).load("https://picsum.photos/350/200").into(iv3);
+        Glide.with(this).load("https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-9/s960x960/51107093_2024212304341197_4647198740765999104_o.jpg?_nc_cat=107&_nc_ohc=xkwHdyhuGL8AX9qVtve&_nc_ht=scontent.fkul15-1.fna&oh=8cf507762379c39b5065d23284ff7a7f&oe=5EC47A45").into(iv2);
+        Glide.with(this).load("https://www.thevocket.com/app/uploads/2018/12/MIB-Cover.jpeg").into(iv3);
 
         Glide.with(this).load("https://is5-ssl.mzstatic.com/image/thumb/Video128/v4/f2/0b/fa/f20bfa07-96dc-e39b-d8db-1c2f8aae8482/source/600x600bb.jpg").into(iv4);
         Glide.with(this).load("https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/c4/2a/ad/c42aadfb-764e-db99-300b-c0888604a06b/pr_source.lsr/600x600bb.jpg").into(iv5);
